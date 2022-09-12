caf130bf4e5db22d524021ac12d4c250293af637
Gustaf Larsson <gustaflarsson1969@icloud.com>
2022-09-12 17:50:27 +0200

    ci/cd

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,22 @@
 import { P2putPixelAddrGenerator } from "coin-canvas-lib";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 
+// eslint-disable-next-line
+const env = process.env;
 export default {
   xLen: 1000,
   yLen: 1000,
-  wsURL: "wss://test-ws.immutable.place/",
-  httpURL: "https://test-api.immutable.place",
-  bitmapURL: "https://test-bmp.immutable.place/",
+  wsURL: env.REACT_APP_CONF_WS_URL!,
+  httpURL: env.REACT_APP_CONF_HTTP_URL!,
+  bitmapURL: env.REACT_APP_CONF_BITMAP_URL!,
   httpRateLimit: 300,
   httpTimeout: 10000,
   bitmapTimeout: 10000,
   reconnectMs: 5000,
   addrGen: new P2putPixelAddrGenerator(
-    "tpc", // Testnet
-    [0xc7, 0x66, 0xce, 0xc1, 0xef] // "canvas00" prefix
-  )
+    env.REACT_APP_CONF_NET_LETTER_CODE!, // Testnet
+    env.REACT_APP_CONF_NET_LETTER_CODE === "tpc"
+      ? [0xc7, 0x66, 0xce, 0xc1, 0xef]
+      : [0xc7, 0x66, 0xce, 0xc1, 0xef] // "canvas00" prefix
+  ),
 };
