803956be50dd8a938216d7ba2d0a7903b6ba6a67
peerchemist <dx.1178@gmail.com>
2022-09-22 14:59:41 +0200

    Update BuyModal.tsx
    somehow gave it a new line here

--- src/components/BuyModal.tsx
+++ src/components/BuyModal.tsx
@@ -115,7 +115,6 @@ export default function BuyModal({
                 >
                   Uniswap
                 </a>
-                
               </li>
             </ul>
           </small>
